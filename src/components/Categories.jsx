e580551c5787f3a405ea82a068463c162bfac6fc
ivan <ivanlisichkin@icloud.com>
2023-07-08 02:59:17 +0400

    feat: add rendering categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,30 +3,22 @@ import { useState } from 'react';
 function Categories() {
   const [activeIndex, setActiveIndex] = useState(0);
 
+  const categories = ['Все', 'Мясные', 'Вегетарианские', 'Гриль', 'Острые', 'Закрытые'];
   const onClickCategory = (index) => {
     setActiveIndex(index);
   };
   return (
     <nav className="categories">
       <ul>
-        <li onClick={() => onClickCategory(0)} className={activeIndex === 0 ? 'active' : ''}>
-          Все
-        </li>
-        <li onClick={() => onClickCategory(1)} className={activeIndex === 1 ? 'active' : ''}>
-          Мясные
-        </li>
-        <li onClick={() => onClickCategory(2)} className={activeIndex === 2 ? 'active' : ''}>
-          Вегетарианская
-        </li>
-        <li onClick={() => onClickCategory(3)} className={activeIndex === 3 ? 'active' : ''}>
-          Гриль
-        </li>
-        <li onClick={() => onClickCategory(4)} className={activeIndex === 4 ? 'active' : ''}>
-          Острые
-        </li>
-        <li onClick={() => onClickCategory(5)} className={activeIndex === 5 ? 'active' : ''}>
-          Закрытые
-        </li>
+        {categories.map((category, index) => {
+          return (
+            <li
+              onClick={() => onClickCategory(index)}
+              className={activeIndex === index ? 'active' : ''}>
+              {category}
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
