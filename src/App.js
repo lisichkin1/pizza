260e7b836e2aaad33868fd7262751c0afce7fcc1
ivan <ivanlisichkin@icloud.com>
2023-07-09 23:32:38 +0400

    feat: add fetch request

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaCard from './components/PizzaCard';
-import pizzas from './assets/pizza.json';
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch('https://64a9d0c38b9afaf4844b1769.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
   return (
     <div className="wrapper">
-      <Header />
       <main className="content">
         <div className="container">
-          <section className="content__top">
-            <Categories />
-            <Sort />
-          </section>
+          <section className="content__top"></section>
           <h2 className="content__title">Все пиццы</h2>
           <section className="content__items">
-            {pizzas.map((item) => {
+            {items.map((item) => {
               return <PizzaCard key={item.id} {...item} />;
             })}
           </section>
