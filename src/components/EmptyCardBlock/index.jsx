7e31cea6ab835605538891a48f66c2ae5b56d930
ivan <ivanlisichkin@icloud.com>
2023-07-12 00:10:25 +0400

    feat:add EmptyCart page

---
+++ src/components/EmptyCardBlock/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function EmptyCartBlock() {
+  return (
+    <div>
+      <h1>
+        <span>&#128546;</span>
+        <br />
+        Ничего не найдено
+      </h1>
+    </div>
+  );
+}
+
+export default EmptyCartBlock;
---
+++ src/pages/EmptyCart.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import EmptyCartBlock from '../components/EmptyCardBlock';
+
+function EmptyCart() {
+  return <EmptyCartBlock />;
+}
+
+export default EmptyCart;
