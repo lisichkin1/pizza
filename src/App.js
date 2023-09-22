b398700fafb749cf018c34de6d7d92fc946ae4b2
ivan <ivanlisichkin@icloud.com>
2023-09-22 19:46:16 +0400

    faet:add redux

--- src/App.js
+++ src/App.js
@@ -5,13 +5,28 @@ import NotFound from './pages/NotFound';
 import { Route, Routes } from 'react-router-dom';
 import Cart from './pages/Cart';
 import { createContext, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './redux/slices/filterSlice';
 
 export const AppContext = createContext();
 function App() {
   const [searchValue, setSearchValue] = useState('');
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <div className="wrapper">
       <AppContext.Provider value={{ searchValue, setSearchValue }}>
+        <div>
+          <div>
+            <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+              Increment
+            </button>
+            <span>{count}</span>
+            <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+              Decrement
+            </button>
+          </div>
+        </div>
         <Header />
         <main className="content">
           <div className="container">
